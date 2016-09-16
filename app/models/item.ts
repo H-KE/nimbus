f6b9f9c86c6e7bf68f48324fb6590e3907f880e5
han <han.ke@betterment.com>
2016-09-16 18:52:22 -0400

    wtf

--- app/models/item.ts
+++ app/models/item.ts
@@ -2,7 +2,8 @@ export class Item {
   id: number;
   name: string;
   img: string;
-  price: number;
+  price: number[];
+  priceLabels: string[];
   description: string;
   thc: number;
   subspecies: string;
--- app/pages/item-details/item-details.ts
+++ app/pages/item-details/item-details.ts
@@ -8,9 +8,18 @@ import { Item } from '../../models/item';
 })
 export class ItemDetailsPage {
   selectedItem: any;
+  quantity: number;
+  quantityLabel: string
+  itemPrice: number;;
 
   constructor(public navCtrl: NavController, navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
+    this.quantity = 0;
+
+    this.itemPrice = this.selectedItem.price[this.quantity];
+    this.quantityLabel = this.selectedItem.priceLabels[this.quantity];
+
+    console.log(this.selectedItem);
   }
 }
