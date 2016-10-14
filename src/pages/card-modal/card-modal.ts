77bf3026c4a621ed29e70cd45d5a12293c017bcf
han <han.ke@betterment.com>
2016-10-13 21:21:56 -0400

    fleshing out checkout page - select address + credit card / add as well

--- src/pages/card-modal/card-modal.ts
+++ src/pages/card-modal/card-modal.ts
@@ -18,69 +18,69 @@ export class CardModalPage {
   expMonthChanged: boolean;
   expYearChanged: boolean;
   errorMessage: string;
-  //
-  // constructor(public navCtrl: NavController,
-  //             public stripeService: StripeService,
-  //             public formBuilder: FormBuilder,
-  //             public viewController: ViewController) {
-  //   this.cardNumberChanged = false;
-  //   this.cvcChanged = false;
-  //   this.expMonthChanged = false;
-  //   this.expYearChanged = false;
-  //
-  //   this.cardForm = formBuilder.group({
-  //     number: ["", Validators.compose([Validators.required, this.checkCreditCardNumber])],
-  //     cvc: ["", Validators.compose([Validators.required, this.checkCVC])],
-  //     exp_month: ["", Validators.required],
-  //     exp_year: ["", Validators.required]
-  //   }, {validator: this.checkExpiryDate('exp_month', 'exp_year')});
-  // }
-  //
-  // checkCreditCardNumber(control: Control) {
-  //   if (!Stripe.card.validateCardNumber(control.value)) {
-  //     return {checkCreditCardNumber: true}
-  //   }
-  //   else {
-  //     return null;
-  //   }
-  // }
-  //
-  // checkCVC(control: Control) {
-  //   if (!Stripe.card.validateCVC(control.value)) {
-  //     return {checkCVC: true}
-  //   }
-  //   else {
-  //     return null;
-  //   }
-  // }
-  //
-  // checkExpiryDate(monthKey, yearKey) {
-  //   return (group: ControlGroup) => {
-  //     let month = group.controls[monthKey];
-  //     let year = group.controls[yearKey];
-  //     if (!Stripe.card.validateExpiry(month.value, year.value)) {
-  //       return year.setErrors({checkExpiryDate: true});
-  //     }
-  //     else {
-  //       return year.setErrors(null);
-  //     }
-  //   }
-  // }
 
-  // // submit() {
-  // //   this.stripeService.createToken(this.cardForm.value)
-  // //     .then(response => {
-  // //       if (response['error']) {
-  // //         this.errorMessage = response['error'].message;
-  // //       }
-  // //       else {
-  // //         console.log(response);
-  // //         this.viewController.dismiss(response);
-  // //       }
-  // //     })
-  // }
-  //
-  // dismiss() {
-  //   this.viewController.dismiss("User dimiss");
-  // }
+  constructor(public navCtrl: NavController,
+              public stripeService: StripeService,
+              public formBuilder: FormBuilder,
+              public viewController: ViewController) {
+    this.cardNumberChanged = false;
+    this.cvcChanged = false;
+    this.expMonthChanged = false;
+    this.expYearChanged = false;
+
+    this.cardForm = formBuilder.group({
+      number: ["", Validators.compose([Validators.required, this.checkCreditCardNumber])],
+      cvc: ["", Validators.compose([Validators.required, this.checkCVC])],
+      exp_month: ["", Validators.required],
+      exp_year: ["", Validators.required]
+    }, {validator: this.checkExpiryDate('exp_month', 'exp_year')});
+  }
+
+  checkCreditCardNumber(control) {
+    if (!Stripe.card.validateCardNumber(control.value)) {
+      return {checkCreditCardNumber: true}
+    }
+    else {
+      return null;
+    }
+  }
+
+  checkCVC(control) {
+    if (!Stripe.card.validateCVC(control.value)) {
+      return {checkCVC: true}
+    }
+    else {
+      return null;
+    }
+  }
+
+  checkExpiryDate(monthKey, yearKey) {
+    return (group) => {
+      let month = group.controls[monthKey];
+      let year = group.controls[yearKey];
+      if (!Stripe.card.validateExpiry(month.value, year.value)) {
+        return year.setErrors({checkExpiryDate: true});
+      }
+      else {
+        return year.setErrors(null);
+      }
+    }
+  }
+
+  submit() {
+    this.stripeService.createToken(this.cardForm.value)
+      .then(response => {
+        if (response['error']) {
+          this.errorMessage = response['error'].message;
+        }
+        else {
+          console.log(response);
+          this.viewController.dismiss(response);
+        }
+      })
+  }
+
+  dismiss() {
+    this.viewController.dismiss("User dimiss");
+  }
 }
--- src/pages/checkout/checkout.ts
+++ src/pages/checkout/checkout.ts
@@ -8,6 +8,9 @@ import { CartService } from '../../providers/cart/cart';
 import { OrderService } from '../../providers/orders/orders';
 import { ProfileService } from '../../providers/profile/profile';
 
+import { AddressModalPage } from '../../pages/address-modal/address-modal';
+import { CardModalPage } from '../../pages/card-modal/card-modal';
+
 
 @Component({
   selector: 'checkout',
@@ -19,13 +22,17 @@ export class CheckoutPage {
   subTotal: number;
   orderTotal: number;
   selectedAddress: any;
+  selectedCard: any;
+  cardOptions: any;
+  addressOptions: any;
+  selectedText: any;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public orderService: OrderService,
               public cartService: CartService,
               public profileService: ProfileService,
-              public modalController: ModalController,
+              public modalCtrl: ModalController,
               public loadingCtrl: LoadingController) {
 
     this.order = null;
@@ -33,14 +40,30 @@ export class CheckoutPage {
     this.subTotal = this.order.total_price;
     this.orderTotal = this.order.total_price;
 
+    this.cardOptions = {
+      title: 'Select a card'
+    };
+    this.addressOptions = {
+      title: 'Select an address'
+    }
+
+    this.loadUser();
+  }
+
+  loadUser() {
+    var loader = this.loadingCtrl.create({});
+    loader.present();
+
     this.profileService.getUser()
       .map(response => response.json())
       .subscribe(
         data => {
           console.log(data);
           this.user = data;
-          this.selectedAddress = JSON.parse(data.address)[0];
-          console.log(JSON.parse(data.address));
+          this.user.addresses = JSON.parse(data.address);
+          this.selectedAddress = this.user.addresses[0];
+          this.selectedCard = this.user.cards[0];
+          loader.dismiss();
         },
         error => {
           console.log(error);
@@ -48,7 +71,57 @@ export class CheckoutPage {
       );
   }
 
+  addAddress() {
+    let addressModal = this.modalCtrl.create(AddressModalPage);
+    addressModal.present();
+    addressModal.onDidDismiss(data => {
+      if (data) {
+        var loader = this.loadingCtrl.create({});
+        loader.present();
+
+        this.user.addresses.push(data);
+        this.selectedAddress = data;
+
+        this.profileService.updateUser({
+          address: JSON.stringify(this.user.addresses)
+        }).then(response => {
+          console.log(response);
+          loader.dismiss()
+        });
+      }
+    });
+  }
+
+  addCreditCard() {
+    let cardModal = this.modalCtrl.create(CardModalPage);
+    cardModal.present();
+    cardModal.onDidDismiss(data => {
+      if (data.card) {
+        var loader = this.loadingCtrl.create({});
+        loader.present();
+
+        this.user.cards.push(data.card);
+        this.selectedCard = data.card;
+
+        this.profileService.updateUser({
+          token: data.id
+        }).then(response => {
+          console.log(response);
+          loader.dismiss()
+        });
+      }
+    });
+  }
+
   placeOrder() {
+    if (!this.selectedAddress) {
+      console.log("NO ADDRESS");
+      return;
+    } else if (!this.selectedCard) {
+      console.log("NO CARD");
+      return;
+    }
+
     this.order.address = JSON.stringify(this.selectedAddress);
     this.order.distribution_channel = "delivery";
 
